feda36d7e4d8f93c6c7697503c2c542282ca5224
jeremias23-guapo <jeremiashoy248@gmail.com>
2026-01-19 21:10:04 -0600

    creando yam 1

--- frontcensomed/src/api/axios.js
+++ frontcensomed/src/api/axios.js
@@ -1,9 +1,10 @@
-// src/api/axios.js
 import axios from 'axios';
 
 const API_URL =
   process.env.REACT_APP_API_URL ||
   'https://repopruebatec-756570331238.us-central1.run.app/api';
+
+console.log('🔥 AXIOS BASE URL EN BUILD:', API_URL);
 
 const instance = axios.create({
   baseURL: API_URL,
