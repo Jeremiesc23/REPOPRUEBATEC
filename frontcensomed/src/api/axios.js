dfa05949bd08978b411cc34d9393c1b5673c77ac
jeremias23-guapo <jeremiashoy248@gmail.com>
2026-01-19 19:44:55 -0600

    Use REACT_APP_API_URL for backend base URL

--- frontcensomed/src/api/axios.js
+++ frontcensomed/src/api/axios.js
@@ -1,10 +1,8 @@
 // src/api/axios.js
 import axios from 'axios';
 
-// Aquí aseguramos que estamos apuntando a localhost:8080
-const API_URL = 'http://localhost:8080/api/';
+const API_URL = process.env.REACT_APP_API_URL;
 
-// Crear la instancia de axios con la URL base
 const instance = axios.create({
     baseURL: API_URL,
     headers: {
@@ -13,3 +11,4 @@ const instance = axios.create({
 });
 
 export default instance;
+
