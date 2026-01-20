66c134e234c53df422a706fcd3944627d9a2eaa1
jeremias23-guapo <jeremiashoy248@gmail.com>
2026-01-19 21:04:38 -0600

    creando yam 1

--- frontcensomed/src/App.js
+++ frontcensomed/src/App.js
@@ -1,26 +1,31 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Usamos Routes y Route de react-router-dom
-import Sidebar from './components/Sidebar'; // Tu Sidebar para navegación
-import ProfesionesPage from './pages/ProfesionPage'; // Página que muestra la lista de profesiones
-import NuevaProfesionPage from './pages/NuevaProfesionPage'; // Página para agregar nueva profesión
-import PersonaPage from './pages/PersonaPage';  // Página que muestra la lista de personas
-import NuevaPersonaPage from './pages/NuevaPersonaPage';  // Página para agregar nueva persona
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import ProfesionesPage from './pages/ProfesionPage';
+import NuevaProfesionPage from './pages/NuevaProfesionPage';
+import PersonaPage from './pages/PersonaPage';
+import NuevaPersonaPage from './pages/NuevaPersonaPage';
 
 function App() {
     return (
         <Router>
             <div style={{ display: 'flex' }}>
-                <Sidebar /> {/* Barra lateral con la navegación */}
+                <Sidebar />
                 <div style={{ flex: 1, padding: 20 }}>
-                    <Routes> {/* Usamos Routes para envolver las rutas */}
-                        <Route path="/profesiones" element={<ProfesionesPage />} /> {/* Usamos element en lugar de component */}
+                    <Routes>
+                        {/* Ruta raíz */}
+                        <Route path="/" element={<Navigate to="/profesiones" />} />
+
+                        {/* Profesiones */}
+                        <Route path="/profesiones" element={<ProfesionesPage />} />
                         <Route path="/nueva-profesion" element={<NuevaProfesionPage />} />
-                         <Route path="/editar-profesion/:id" element={<NuevaProfesionPage />} />
-                         <Route path="/personas" element={<PersonaPage />} />
-                <Route path="/nueva-persona" element={<NuevaPersonaPage />} />  
-                 <Route path="/editar-persona/:id" element={<NuevaPersonaPage  />} /> {/* Ruta dinámica con ID */}
-                 
+                        <Route path="/editar-profesion/:id" element={<NuevaProfesionPage />} />
+
+                        {/* Personas */}
+                        <Route path="/personas" element={<PersonaPage />} />
+                        <Route path="/nueva-persona" element={<NuevaPersonaPage />} />
+                        <Route path="/editar-persona/:id" element={<NuevaPersonaPage />} />
                     </Routes>
                 </div>
             </div>
@@ -29,3 +34,4 @@ function App() {
 }
 
 export default App;
+
